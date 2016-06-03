e4248e8ee3dfbfa049cdcacda8b258d5857351f0
Troy Sweeney <troysweeney@programmer.net>
2016-06-03 12:41:09 -0700

    Just thought I'd add an initializer to the program...

--- Core/CoreMain.java
+++ Core/CoreMain.java
@@ -1,1 +1,18 @@
+package Core;
 
+public class CoreMain {
+
+	/**
+	 * @param args
+	 */
+	public static void init(int retval) {
+		
+		switch(retval)
+		{
+			// Depending on the retval value, the initalizer will return different values or results, sometimes errors.
+			
+		}
+		// We have yet to add stuff here!
+	}
+
+}
