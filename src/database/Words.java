079e9e4fd210f12b8a0fb1bc6408a644d2617094
Troy Sweeney <ts64bit@tuta.io>
2017-06-03 20:41:02 -0700

    Modified dictionary

--- src/database/Words.java
+++ src/database/Words.java
@@ -20,15 +20,9 @@ public class Words
 		char alpha[] = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
 	}
 
-	public class 中文
+	public class UnicodeDefines
 	{
-		public static final String 创建 = "create";
-		public static final String 随机存取存储器  = "ram";
-		public static final String 结束           = "end";
-		public static final String 帮帮我          = "help";
-		public static final String 你好             = "hello";
-		public static final String 平                = "ping";
-		public static final String 安全壳              = "ssh";
+		String std = "UTF-8";
+	        char SpecialSymbols  = { 'å', '∫', 'ç', '∂', '´', 'ƒ', '©', '˙', 'ˆ', '∆', '˚', '¬', 'µ', '˜', 'ø', 'π', 'œ', '®', 'ß', '†', '¨', '√', '∑', '≈', '\', 'Ω', 'æ', '…', '¡', '™', '£', '¢', '∞', '§', '¶', '•', 'ª', '‡', '›', '‹', '⁄', '°', 'ﬂ' };
 	}
-
 }
